9ff4ece82f573ea92eac3cfe3ea75a7393413f9f
Rayam Fonsêca <133929446+VoupraSP@users.noreply.github.com>
2025-03-03 11:16:06 -0300

    adding first js

---
+++ script.js
@@ -0,0 +1,26 @@
+const nav = document.querySelector("nav");
+const produtos = document.querySelector(".produtos a");
+
+console.log(nav);
+console.log(produtos);
+
+const name = "Rayam";
+const lastName = 'Fonseca';
+const fullName = name +' '+lastName;
+
+console.log(fullName);
+
+
+const livro = {
+    nome: 'A hora da estrela',
+    ano: 1977,
+    autor: 'Clarice Lispector'
+}
+
+console.log(livro);
+console.log(2025 - livro.ano);
+console.log(lastName.length);
+
+const conteudoTexto = document.querySelector('p');
+
+console.log(conteudoTexto.innerText); 
