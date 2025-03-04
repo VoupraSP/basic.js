a327828c2ffaf2af9677487a35fa48a51d7e7cc2
Rayam Fonsêca <133929446+VoupraSP@users.noreply.github.com>
2025-03-04 12:09:57 -0300

    adding first sample function

--- 2 - funcoes/script.js
+++ 2 - funcoes/script.js
@@ -4,3 +4,20 @@ function logCurso() {
 }
 
 logCurso();
+
+function livro(nome, ano, autor) {
+    const nomeMaior = nome.toUpperCase();
+    const totalAno = 2050 - ano;
+    const frase = nome + " por " + autor;
+
+    const objeto = {
+        nome: nomeMaior,
+        totalAno,
+        frase,
+    };
+    return objeto;
+}
+
+const retornoFunc = livro("O Senhor dos Anéis", 1954, "J. R. R. Tolkien");
+
+console.log(retornoFunc.frase);
